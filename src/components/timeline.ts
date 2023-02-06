30bc9b8402c1573ef16da2472b87a23b928568fe
AsPulse <aspulse.x@gmail.com>
2023-02-06 20:19:50 +0900

    Add: Timeline Status

--- src/components/timeline.ts
+++ src/components/timeline.ts
@@ -2,10 +2,13 @@ import { ChannelMessageEvent } from '../api.ts';
 import { TUIArea, TUIComponent, TUIParent } from '../tui/index.ts';
 import { uiString } from '../tui/string.ts';
 
+type StatusType = { now: string, left: string | null, right: string | null, };
+
 export class Timeline implements TUIComponent {
 
   constructor(public parent: TUIParent){}
 
+  status: StatusType = { now: 'Local', left: 'Home', right: 'Social' };
   notes: ChannelMessageEvent[] = [];
 
   async addNote(note: ChannelMessageEvent) {
@@ -13,7 +16,18 @@ export class Timeline implements TUIComponent {
     await this.parent.render();
   }
 
+  async resetTimeline(newStatus: StatusType) {
+    this.notes = [];
+    this.status = newStatus;
+    await this.parent.render();
+  }
+
+
   render(area: TUIArea) {
+
+    const now = uiString([{ text: `${this.status.now} TIMELINE`, bold: true }]);
+    const left = uiString([{ text: `<[h] ${this.status.left}` }]);
+    const right = uiString([{ text: `${this.status.right} [l]>` }]);
 
     return Promise.resolve([
       ...[...this.notes].reverse().flatMap((v, i) => ([
@@ -37,10 +51,22 @@ export class Timeline implements TUIComponent {
         }
       ])),
       {
+        x: area.x + (area.w - now.length) / 2,
+        y: area.y,
+        z: 3,
+        content: [now]
+      },
+      {
         x: area.x,
         y: area.y,
         z: 2,
-        content: [uiString([{ text: 'LOCAL TIMELINE', bold: true }])]
+        content: this.status.left !== null ? [left] : []
+      },
+      {
+        x: area.w - area.x - right.length - 1,
+        y: area.y,
+        z: 2,
+        content: this.status.right !== null ? [right] : []
       }
     ]);
   }
