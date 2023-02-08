89209e35e47c58cba9721566f63df72511ad1a83
see2et <>
2023-02-09 02:41:40 +0900

    テストを罫線とかに対応させた

--- src/tui/string.test.ts
+++ src/tui/string.test.ts
@@ -1,7 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { is2Byte } from "./string.ts";
 
-// ┌┐├┤╭╮╰╯└┘│─…'.
 Deno.test("is2Byte() given numbers", () => {
   {
     const actual = is2Byte("2");
@@ -36,7 +35,12 @@ Deno.test("is2Byte() given '...'", () => {
 
 Deno.test("is2Byte() given some ruled lines and so on", () => {
   {
-    const actual = is2Byte("...");
+    const actual = is2Byte("┌");
+    const expected = false;
+    assertEquals(actual, expected);
+  }
+  {
+    const actual = is2Byte("┐");
     const expected = false;
     assertEquals(actual, expected);
   }
