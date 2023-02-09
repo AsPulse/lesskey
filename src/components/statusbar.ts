a69c6bc11734682448a0a383bf9eaba5424d2080
AsPulse <aspulse.x@gmail.com>
2023-02-09 11:15:46 +0900

    Add: statusbar text

--- src/components/statusbar.ts
+++ src/components/statusbar.ts
@@ -5,9 +5,15 @@ export class StatusBar implements TUIComponent {
   constructor(public parent: TUIParent) {}
 
   id: null | string = null;
+  text = '';
 
   async setId(id: string) {
     this.id = id;
+    await this.parent.render();
+  }
+  
+  async setText(content: string) {
+    this.text = content;
     await this.parent.render();
   }
 
@@ -18,32 +24,45 @@ export class StatusBar implements TUIComponent {
       true,
     );
 
+    const username = 
+      uiString(
+        [{
+          text: this.id === null
+            ? ' LessKey '
+            : ` LessKey [@${this.id}@misskey.io] `,
+          backgroundColor: [255, 56, 139],
+          foregroundColor: [255, 255, 255],
+          bold: true,
+        }],
+        area.w,
+        true,
+      );
+
     return Promise.resolve([
       {
-        x: 0,
+        x: area.x,
         y: area.h - area.y - 1,
-        z: 5,
-        content: [
-          uiString(
-            [{
-              text: this.id === null
-                ? ' LessKey '
-                : ` LessKey [@${this.id}@misskey.io] `,
-              backgroundColor: [255, 56, 139],
-              foregroundColor: [255, 255, 255],
-              bold: true,
-            }],
-            area.w,
-            true,
-          ),
-        ],
+        z: 11,
+        content: [username],
       },
       {
         x: area.w - area.x - howToQuit.length - 1,
         y: area.h - area.y - 1,
-        z: 5,
+        z: 12,
         content: [howToQuit],
       },
+      {
+        x: area.x + username.length + 1,
+        y: area.h - area.y - 1,
+        z: 11,
+        content: [uiString([{ text: this.text }], area.w, true)]
+      },
+      {
+        x: area.x,
+        y: area.h - area.y - 1,
+        z: 10,
+        content: [uiString([{ text: ' '.repeat(area.w) }], area.w, true)],
+      }
     ]);
   }
 }
